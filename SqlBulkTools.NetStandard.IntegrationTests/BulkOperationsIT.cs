6650b4d092934955046e39b981275478c08954dc
Brian Brooks <Brian.Brooks@Lifemark.ca>
2023-10-02 17:58:10 -0400

    Update Unit tests

--- SqlBulkTools.NetStandard.IntegrationTests/BulkOperationsIT.cs
+++ SqlBulkTools.NetStandard.IntegrationTests/BulkOperationsIT.cs
@@ -7,7 +7,7 @@ using SqlBulkTools.TestCommon.Model;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Transactions;
